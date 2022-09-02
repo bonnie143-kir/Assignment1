b6ec2d3b2bb9068b511a03065821f7852076a154
Bongraoi Arebaio <bongraoi.arebaio@griffithuni.edu.au>
2022-09-02 21:12:25 +1000

    Added routes and routerlinks for login, user and superadmin

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { SuperAdminComponent } from './super-admin/super-admin.component';
+import { GroupAdminComponent } from './group-admin/group-admin.component';
+import { GroupAssistComponent } from './group-assist/group-assist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'superAdmin', component: SuperAdminComponent},
+  {path: 'groupAdmin', component: GroupAdminComponent},
+  {path: 'groupAssist', component: GroupAssistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/super-admin/super-admin.component.ts
+++ frontend/src/app/super-admin/super-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-super-admin',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuperAdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
