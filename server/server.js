afb43b4c22647fc55b30e1a973613f6b183a96c7
Bongraoi Arebaio <bongraoi.arebaio@griffithuni.edu.au>
2022-09-03 11:01:57 +1000

    Fixed routing problem

--- server/server.js
+++ server/server.js
@@ -3,3 +3,10 @@ var app = express();
 var http = require('http').Server(app);
 
 app.use(express.static(__dirname + '/dist/phase1/'));
+
+app.listen(3000, ()=>{
+    var d = new Date();
+    var n = d.getHours();
+    var m = d.getMinutes();
+    console.log('Server is listening at: ' + n + ':' + m);
+})
