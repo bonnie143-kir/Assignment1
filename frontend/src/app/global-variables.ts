f3ef38a273d8f79ab8fc1df1c76ff19288bbcb0d
Bongraoi Arebaio <bongraoi.arebaio@griffithuni.edu.au>
2022-09-03 22:52:18 +1000

    Added httpclient module to app.module.ts

---
+++ frontend/src/app/global-variables.ts
@@ -0,0 +1,5 @@
+export const users = [
+    {'username': 'bongii', 'birthdate': '14 Jan', 'age': 23, 'email': 'bongii@outmail.com', 'password': '1234', 'valid': true},
+    {'username': 'bonnie', 'birthdate': '14 Jan', 'age': 23, 'email': 'bonnie@outmail.com', 'password': '567', 'valid': true},
+    {'username': 'bong', 'birthdate': '14 Jan', 'age': 23, 'email': 'bong@outmail.com', 'password': 'abcd', 'valid': true}
+];
