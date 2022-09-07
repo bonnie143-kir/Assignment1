e1ce57972c5be680db350e6c1503393566053d05
Bongraoi Arebaio <bongraoi.arebaio@griffithuni.edu.au>
2022-09-08 04:31:01 +1000

    Imported necessary modules for http communication

--- frontend/src/app/super-admin/super-admin.component.ts
+++ frontend/src/app/super-admin/super-admin.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { response } from 'express';
+
+const url = 'http://localhost:3000';
 
 @Component({
   selector: 'app-super-admin',
@@ -8,8 +16,8 @@ import { Router } from '@angular/router';
 })
 export class SuperAdminComponent implements OnInit {
 
-  constructor(private router: Router) { }
-
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
+  
   ngOnInit(): void {
   }
 
