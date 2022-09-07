2c41ae2998e543602b6da5ee7aad7500885de01f
Bongraoi Arebaio <bongraoi.arebaio@griffithuni.edu.au>
2022-09-08 04:44:11 +1000

    Imported the new component into app routing module component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { SuperAdminComponent } from './super-admin/super-admin.component';
 import { GroupAdminComponent } from './group-admin/group-admin.component';
 import { GroupAssistComponent } from './group-assist/group-assist.component';
 import { HomeComponent } from './home/home.component';
+import { NormalUserComponent } from './normal-user/normal-user.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path: 'super-admin', component: SuperAdminComponent},
   {path: 'group-admin', component: GroupAdminComponent},
   {path: 'group-assist', component: GroupAssistComponent},
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'normalUser', component:NormalUserComponent}
 ];
 
 @NgModule({
